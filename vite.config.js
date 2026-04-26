200c53a2e128c44d8f93eec33aee5d661c68d197
Apple <apple@imac-de-apple.home>
2026-04-26 23:13:35 +0200

    ajout PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,32 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'NutriVie',
+        short_name: 'NutriVie',
+        description: 'Diététique fondée sur les preuves',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-icon-192x192.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
